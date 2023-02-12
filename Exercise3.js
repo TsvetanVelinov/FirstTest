42afd38e992c58b9ed305b0d76a250365cd04f3c
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2023-02-12 13:39:28 +0200

    FirstTest
    Test

---
+++ Exercise3.js
@@ -0,0 +1,15 @@
+function demo(input) {
+    let NumberSheets = Number(input[0]);
+    let Hours = Number(input[1]);
+    let Days = Number(input[2]);
+
+    let sum = (NumberSheets / 20)
+
+
+    console.log(NumberSheets)
+
+
+
+
+}
+demo(["212 ", "20 ", "2 "])
